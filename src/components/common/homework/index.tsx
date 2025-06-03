e0a506e4893e691cc9874c9b58f4b71f91b4ff9c
recepasl44 <contact@recepaslan.com.tr>
2025-06-03 17:17:50 +0300

    style: align homework UI with pollingManagement

--- src/components/common/homework/index.tsx
+++ src/components/common/homework/index.tsx
@@ -1,6 +1,7 @@
 /* components/common/homework/index.tsx */
 import React, { useState } from 'react';
 import TabsContainer from './components/organisms/TabsContainer';
+import Pageheader from '../../page-header/pageheader';
 
 import PlannedAssignmentsTable from './pages/plannedAssignments/table';
 import DefiningHomeworkPage from './pages/assignmentsDefinition/table';
@@ -12,16 +13,51 @@ const HomeworkTrackingPage: React.FC = () => {
     const [activeIdx, setActiveIdx] = useState(0);
 
     const tabsConfig = [
-        { label: 'Planlanan Ödevler', content: <PlannedAssignmentsTable /> },
-        { label: 'Ödev Tanımlama', content: <DefiningHomeworkPage /> },
-        { label: 'Ödev Kontrolü', content: <AssignmentsCheckTable /> },
-        { label: 'Ödev Listesi', content: <AssignmentsListTable /> },
-        { label: 'Ödev Sayıları', content: <AssignmentsCountPage /> },
+        {
+            label: 'Planlanan Ödevler',
+            content: <PlannedAssignmentsTable />,
+            activeBgColor: '#5C67F7',
+            activeTextColor: '#FFFFFF',
+            passiveBgColor: '#5C67F726',
+            passiveTextColor: '#5C67F7',
+        },
+        {
+            label: 'Ödev Tanımlama',
+            content: <DefiningHomeworkPage />,
+            activeBgColor: '#5C67F7',
+            activeTextColor: '#FFFFFF',
+            passiveBgColor: '#5C67F726',
+            passiveTextColor: '#5C67F7',
+        },
+        {
+            label: 'Ödev Kontrolü',
+            content: <AssignmentsCheckTable />,
+            activeBgColor: '#5C67F7',
+            activeTextColor: '#FFFFFF',
+            passiveBgColor: '#5C67F726',
+            passiveTextColor: '#5C67F7',
+        },
+        {
+            label: 'Ödev Listesi',
+            content: <AssignmentsListTable />,
+            activeBgColor: '#5C67F7',
+            activeTextColor: '#FFFFFF',
+            passiveBgColor: '#5C67F726',
+            passiveTextColor: '#5C67F7',
+        },
+        {
+            label: 'Ödev Sayıları',
+            content: <AssignmentsCountPage />,
+            activeBgColor: '#5C67F7',
+            activeTextColor: '#FFFFFF',
+            passiveBgColor: '#5C67F726',
+            passiveTextColor: '#5C67F7',
+        },
     ];
 
     return (
         <div className="px-4">
-            <h5>Ödev Takip</h5>
+            <Pageheader title="Ödev Yönetimi" currentpage="Ödev Takip" />
 
             <TabsContainer
                 tabs={tabsConfig}
