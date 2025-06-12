32d85b662e853e0b04c47df0a5ef39d397447810
recepasl44 <contact@recepaslan.com.tr>
2025-06-12 14:51:30 +0300

    fix: return correct type for credit card detail

--- src/slices/creditCard/detail/thunk.tsx
+++ src/slices/creditCard/detail/thunk.tsx
@@ -1,16 +1,16 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axiosInstance from "../../../services/axiosClient";
 import { CREDIT_CARD } from "../../../helpers/url_helper";
-import { CreditCardListStatus } from "../../../enums/creditCard/list";
+import { ICreditCard } from "../../../types/creditCard/list";
 
-export const fetchCreditCard = createAsyncThunk<CreditCardListStatus, number>(
+export const fetchCreditCard = createAsyncThunk<ICreditCard, number>(
   "creditCard/fetchCreditCard",
   async (creditCardId, { rejectWithValue }) => {
     try {
       const response = await axiosInstance.get(
         `${CREDIT_CARD}/${creditCardId}`
       );
-      return response.data.data as CreditCardListStatus;
+      return response.data.data as ICreditCard;
     } catch (error: any) {
       return rejectWithValue(
         error.response?.data?.message || "Fetch credit card failed"
