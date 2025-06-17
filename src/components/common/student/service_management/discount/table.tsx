c49dd5ec83f79a3a0793763d943aa387e810f8d8
Melike Aktürk <meliikeakturk1@gmail.com>
2025-06-17 13:48:58 +0300

    bhn

--- src/components/common/student/service_management/discount/table.tsx
+++ src/components/common/student/service_management/discount/table.tsx
@@ -1,4 +1,3 @@
-// src/pages/discounts/table.tsx
 import { useState, useMemo, useEffect } from "react";
 import ReusableTable, {
   ColumnDefinition,
@@ -10,6 +9,7 @@ import { DiscountData } from "../../../../../types/discounts/list";
 import { formatCurrency } from "../../../../../utils/formatters";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useUpdateQueryParamsFromFilters } from "../../../../hooks/utilshooks/useUpdateQueryParamsFromFilters";
+import { Form } from "react-bootstrap";
 
 interface DiscountTableProps {
   serviceId?: number;
@@ -23,70 +23,62 @@ type QueryParams = {
 
 export default function DiscountTable({ serviceId }: DiscountTableProps) {
   const navigate = useNavigate();
+  const { state } = useLocation() as {
+    state?: { service_id?: number; enabled?: boolean };
+  };
+  const currentServiceId = serviceId || state?.service_id;
+
   const [name, setName] = useState("");
-  const [inputName, setInputName] = useState(""); // UI için
+  const [inputName, setInputName] = useState("");
   const debouncedName = useDebounce<string>(inputName, 500);
   const [page, setPage] = useState(1);
   const [pageSize, setPageSize] = useState(10);
   const [enabled, setEnabled] = useState(false);
-  const { deleteExistingDiscount } = useDiscountDelete();
-  const { state } = useLocation() as {
-    state?: { service_id?: number; enabled?: boolean };
-  };
-  // serviceId prop'unu veya route state'inden gelen service_id'yi kullan
-  const currentServiceId = serviceId || state?.service_id;
+  const [filtersEnabled, setFiltersEnabled] = useState({ name: false });
 
-  // Hem prop hem de state değiştiğinde tabloyu güncelle
+  const { deleteExistingDiscount } = useDiscountDelete();
+
   useEffect(() => {
     if (currentServiceId) {
       setEnabled(true);
     }
   }, [currentServiceId]);
-  const [filtersEnabled, setFiltersEnabled] = useState({
-    name: false,
-  });
 
   useEffect(() => {
     const params = new URLSearchParams(location.search);
     if (params.get("discount_name")) setName(params.get("discount_name") || "");
   }, [location.search]);
 
-  const filterState = useMemo(
-    () => ({
-      name: name,
-    }),
-    [name]
-  );
+  const handleNameInputChange = (val: string) => {
+    setInputName(val);
+    setFiltersEnabled((prev) => ({ ...prev, name: true }));
+
+    if (val === "") {
+      setName("");
+    }
+  };
 
   useEffect(() => {
     if (debouncedName) {
-      // name güncellemesi
       setName(debouncedName);
-      // API isteği için filtersEnabled güncellenmesi
-      setFiltersEnabled((prev) => ({
-        ...prev,
-        name: true,
-      }));
+      setFiltersEnabled((prev) => ({ ...prev, name: true }));
     }
   }, [debouncedName]);
+
+  const filterState = useMemo(() => ({ name: name }), [name]);
 
   const updateQueryParams = (params: QueryParams) => {
     const query = new URLSearchParams();
     if (params.name) query.set("discount_name", params.name);
-
     query.set("discount_page", String(page));
-    query.set("discount_page", String(pageSize));
-
+    query.set("discount_pageSize", String(pageSize));
     navigate(`?${query.toString()}`);
   };
 
   useUpdateQueryParamsFromFilters<QueryParams>(filterState, updateQueryParams);
 
   const questionParams = useMemo(
-    () => ({
-      enabled: true,
-      name: name,
-    }),
+    () => ({ enabled: true, name: name }),
     [name]
   );
 
@@ -97,94 +89,76 @@ export default function DiscountTable({ serviceId }: DiscountTableProps) {
     }
   }, [name]);
 
-  const { discountsData, loading, error, totalPages, totalItems } =
-    useDiscountsTable({
-      enabled: enabled,
-      page,
-      pageSize,
-      service_id: currentServiceId,
-      questionParams,
-      name: name,
-    });
+  const {
+    discountsData,
+    loading,
+    error,
+    totalPages,
+    totalItems,
+  } = useDiscountsTable({
+    enabled: enabled,
+    page,
+    pageSize,
+    service_id: currentServiceId,
+    questionParams,
+    name: name,
+  });
 
-  // Filtre Tablosu
-  const filters = useMemo(() => {
-    return [
-      {
-        key: "name",
-        label: "İndirim Adı",
-        value: inputName,
-        placeholder: "İndirim adı...",
-        type: "text" as const,
-        onChange: (val: any) => {
-          setInputName(val);
-          if (val) {
-            const matchedDiscount = discountsData?.find(
-              (item: any) => item.name.toLowerCase() === val.toLowerCase()
-            );
-            if (matchedDiscount) {
-              if (matchedDiscount.name) {
-                setName(matchedDiscount.name);
-              }
-            }
-          } else {
-            setName("");
-          }
-        },
-        isEnabled: filtersEnabled.name,
-      },
-    ];
-  }, [inputName]);
-
-  // ANa tablo
-  const columns: ColumnDefinition<DiscountData>[] = useMemo(() => {
-    return [
-      {
-        key: "name",
-        label: "Ad",
-        render: (row) => row.name,
-      },
-      {
-        key: "amount",
-        label: "Tutar/Oran",
-        render: (row) =>
-          // discount_type = 0 => Yüzde / discount_type=1 => Tutar
-          row.discount_type === 1
-            ? formatCurrency(row.amount)
-            : `%${row.amount}`,
-      },
-      {
-        key: "discount_type",
-        label: "Tip",
-        render: (row) => (row.discount_type === 0 ? "Yüzde(%)" : "Tutar(+)"),
-      },
-      {
-        key: "actions",
-        label: "İşlemler",
-        render: (row, openDeleteModal) => (
-          <div className="d-flex gap-2">
-            <button
-              className="btn btn-icon btn-sm btn-info-light rounded-pill"
-              onClick={() => {
-                navigate(`/discounts/crud/${row.id}`);
-              }}
-            >
-              <i className="ti ti-pencil"></i>
-            </button>
-            <button
-              className="btn btn-icon btn-sm btn-danger-light rounded-pill"
-              onClick={() => openDeleteModal && openDeleteModal(row)}
-            >
-              <i className="ti ti-trash"></i>
-            </button>
-          </div>
-        ),
-      },
-    ];
-  }, []);
+  const columns: ColumnDefinition<DiscountData>[] = useMemo(() => [
+    {
+      key: "name",
+      label: "Ad",
+      render: (row) => row.name,
+    },
+    {
+      key: "amount",
+      label: "Tutar/Oran",
+      render: (row) =>
+        row.discount_type === 1
+          ? formatCurrency(row.amount)
+          : `%${row.amount}`,
+    },
+    {
+      key: "discount_type",
+      label: "Tip",
+      render: (row) => (row.discount_type === 0 ? "Yüzde(%)" : "Tutar(+)"),
+    },
+    {
+      key: "actions",
+      label: "İşlemler",
+      render: (row, openDeleteModal) => (
+        <div className="d-flex gap-2">
+          <button
+            className="btn btn-icon btn-sm btn-info-light rounded-pill"
+            onClick={() => {
+              navigate(`/discounts/crud/${row.id}`);
+            }}
+          >
+            <i className="ti ti-pencil"></i>
+          </button>
+          <button
+            className="btn btn-icon btn-sm btn-danger-light rounded-pill"
+            onClick={() => openDeleteModal && openDeleteModal(row)}
+          >
+            <i className="ti ti-trash"></i>
+          </button>
+        </div>
+      ),
+    },
+  ], []);
 
   return (
     <div>
+      <Form.Group className="mb-3">
+        <Form.Label>İndirim Adı</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="İndirim adı..."
+          value={inputName}
+          onChange={(e) => handleNameInputChange(e.target.value)}
+        />
+      </Form.Group>
+
       <ReusableTable<DiscountData>
         columns={columns}
         data={discountsData}
@@ -193,7 +167,7 @@ export default function DiscountTable({ serviceId }: DiscountTableProps) {
         currentPage={page}
         totalPages={totalPages}
         totalItems={totalItems}
-        filters={filters}
+        filters={[]} // filtre alanını artık dışta sunduğumuz için boş geçiyoruz
         onPageChange={(newPage) => setPage(newPage)}
         pageSize={pageSize}
         tableMode="multi"
@@ -205,7 +179,9 @@ export default function DiscountTable({ serviceId }: DiscountTableProps) {
           });
         }}
         onDeleteRow={(row) => deleteExistingDiscount(row.id)}
-        deleteMessage={(row) => `${row.name} adlı indirimi silmek istediğinize emin misiniz?`}
+        deleteMessage={(row) =>
+          `${row.name} adlı indirimi silmek istediğinize emin misiniz?`
+        }
         deleteCancelButtonLabel="Vazgeç"
         deleteConfirmButtonLabel="Sil"
       />
