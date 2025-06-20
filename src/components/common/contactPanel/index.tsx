43256036f60457b44d4acbe9582227145230a1f9
Melike Aktürk <meliikeakturk1@gmail.com>
2025-06-20 17:28:15 +0300

    bhn

--- src/components/common/contactPanel/index.tsx
+++ src/components/common/contactPanel/index.tsx
@@ -8,49 +8,49 @@ import SmsTable from './pages/sms/table';
 import EmailTable from './pages/e-mail/table';
 
 const ContactPanelIndex: React.FC = () => {
-  const [, setActiveIdx] = useState<number>(0);
+    const [, setActiveIdx] = useState<number>(0);
 
-  const tabs = [
-    {
-      label: 'G\u00fcncel B\u00fclten',
-      content: <CurrentNewsletterTable />,
-      activeBgColor: '#5C67F7',
-      activeTextColor: '#FFFFFF',
-      passiveBgColor: '#5C67F726',
-      passiveTextColor: '#5C67F7',
-    },
-    {
-      label: 'Bildirimler',
-      content: <NotificationsTable />,
-      activeBgColor: '#5C67F7',
-      activeTextColor: '#FFFFFF',
-      passiveBgColor: '#5C67F726',
-      passiveTextColor: '#5C67F7',
-    },
-    {
-      label: 'SMS',
-      content: <SmsTable />,
-      activeBgColor: '#5C67F7',
-      activeTextColor: '#FFFFFF',
-      passiveBgColor: '#5C67F726',
-      passiveTextColor: '#5C67F7',
-    },
-    {
-      label: 'E-Posta',
-      content: <EmailTable />,
-      activeBgColor: '#5C67F7',
-      activeTextColor: '#FFFFFF',
-      passiveBgColor: '#5C67F726',
-      passiveTextColor: '#5C67F7',
-    },
-  ];
+    const tabs = [
+        {
+            label: 'G\u00fcncel B\u00fclten',
+            content: <CurrentNewsletterTable />,
+            activeBgColor: '#5C67F7',
+            activeTextColor: '#FFFFFF',
+            passiveBgColor: '#5C67F726',
+            passiveTextColor: '#5C67F7',
+        },
+        {
+            label: 'Bildirimler',
+            content: <NotificationsTable />,
+            activeBgColor: '#5C67F7',
+            activeTextColor: '#FFFFFF',
+            passiveBgColor: '#5C67F726',
+            passiveTextColor: '#5C67F7',
+        },
+        {
+            label: 'SMS',
+            content: <SmsTable />,
+            activeBgColor: '#5C67F7',
+            activeTextColor: '#FFFFFF',
+            passiveBgColor: '#5C67F726',
+            passiveTextColor: '#5C67F7',
+        },
+        {
+            label: 'E-Posta',
+            content: <EmailTable />,
+            activeBgColor: '#5C67F7',
+            activeTextColor: '#FFFFFF',
+            passiveBgColor: '#5C67F726',
+            passiveTextColor: '#5C67F7',
+        },
+    ];
 
-  return (
-    <div>
-      <Pageheader title="\u0130leti\u015fim Paneli" currentpage="\u0130leti\u015fim Paneli" />
-      <TabsContainer tabs={tabs} onTabChange={(idx) => setActiveIdx(idx)} />
-    </div>
-  );
+    return (
+        <div>
+            <Pageheader title="İletişim  Paneli" currentpage="\u0130leti\u015fim Paneli" />
+            <TabsContainer tabs={tabs} onTabChange={(idx) => setActiveIdx(idx)} />
+        </div>
+    );
 };
 
 export default ContactPanelIndex;
