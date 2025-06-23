c7c698220051288ffeddae8d147b27acecdf2dad
Melike Aktürk <meliikeakturk1@gmail.com>
2025-06-23 11:34:49 +0300

    bg

--- src/components/common/contactPanel/pages/sms/crud.tsx
+++ src/components/common/contactPanel/pages/sms/crud.tsx
@@ -170,7 +170,7 @@ export default function SmsCrud() {
         } else if (mode === 'edit' && id) {
             await updateExistingNotification({ notificationId: Number(id), payload: payload as any });
         }
-        navigate(`${import.meta.env.BASE_URL}contact-panel/sms`);
+        navigate(`${import.meta.env.BASE_URL}contact-panel`);
     };
 
     const isLoading =
